3e5937894e4dce9e6f9c02d5f6999726388dc632
amal18163 <atidjani@wellesley.edu>
2016-11-19 16:38:47 -0500

    Updated Our Story & Who We Are Content

--- js/load.js
+++ js/load.js
@@ -10,4 +10,4 @@ $("#header_here").load("index.html header");
 /*Load header HTML and use run_header to load JS animations*/
 $("#footer_here").load("index.html footer", animateLinks);
 
-$("#mainten_here").load("who_we_are.html #mainten_content");
+$("#mainten_here").load("news.html #mainten_content");
